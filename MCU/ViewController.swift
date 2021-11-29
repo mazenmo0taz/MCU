d1d8b7442294a2552a17e8ab7a79fa9cbc60d68c
islamovic <islam.abo.elsoud@gmail.com>
2021-11-29 19:39:10 +0200

    clean the code base just a bit

--- MCU/ViewController.swift
+++ MCU/ViewController.swift
@@ -11,15 +11,15 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         let m = MarvelData()
         let params = ["limit": 100, "offset": 300]
+        
         m.request(with: "/v1/public/characters?", parameters: params, method: .get) { data in
             let chars = try? JSONDecoder().decode(CharacterDataResult.self, from: data)
             print(chars?.data.results ??  "error while fetching chars data")
         } failure: { error in
             print(error.localizedDescription)
         }
-
     }
 }
-
